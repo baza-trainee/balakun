4fc526f9252d748c4b6a8c77ffee3d3cd6a5e6ec
Timka <timkaanddoge@gmail.com>
2024-03-17 17:59:29 +0200

    feat: added internationalization

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -1,14 +1,24 @@
 import "./fonts/fonts";
-import { roboto } from "./globals.css";
+import "./globals.css";
+import { roboto } from "./fonts/fonts";
+
+
+import i18nConfig from "@/i18nConfig";
 
 export const metadata = {
   title: "Balakun",
   description: "Speak me",
 };
 
-export default function RootLayout({ children }) {
+//To generate static routes for a given set of locales
+export function generateStaticParams() {
+  return i18nConfig.locales.map(locale => ({ locale }));
+}
+
+
+export default function RootLayout({ children,params: { lang } }) {
   return (
-    <html lang="en">
+    <html lang={lang}>
       <body className={roboto.className}>{children}</body>
     </html>
   );
--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,6 +1,10 @@
+import initTranslations from "../i18n";
+import TranslationsProvider from "./components/Internationalization/TranslationsProvider/TranslationsProvider";
+
 import { About } from "./components/About";
 import { Achivements } from "./components/Achivements";
 import { Community } from "./components/Community";
+
 import { Experience } from "./components/Experience";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
@@ -10,11 +14,25 @@ import { Questions } from "./components/Questions";
 import { Team } from "./components/Team";
 import { YouTube } from "./components/YouTube";
 
+/*This element is example, It can switch language*/
+//import LanguageChanger from "./components/Internationalization/LanguageChanger/LanguageChanger";
+//import ExampleClient from "./components/Internationalization/ExampleClient/ExampleClient";
 
-export default function Home() {
+//file's name, you can add other files in other components  
+const i18nNamespaces = ['translation'];
+
+export default async function Home({params:{lang}}) {
+/*You can run this function to change language on the server components*/
+  const {t, resources }= await initTranslations(lang,i18nNamespaces)
   return (
-    <>
+    <TranslationsProvider
+    namespaces={i18nNamespaces}
+    locale={lang}
+    resources={resources}
+    >
       <Header />
+
+      {/* <LanguageChanger/> */}
       <main>
         <Hero />
         <About />
@@ -25,8 +43,11 @@ export default function Home() {
         <Experience />
         <Questions />
         <Community />
+        {/* 
+        *You'll see how change language in this components *
+        <ExampleClient/> */}
       </main>
       <Footer />
-    </>
+      </TranslationsProvider>
   );
 };
