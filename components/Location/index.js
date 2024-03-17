85777e05ec80e0a7618fccc9a29b303a774daa8d
Viktor Filippov <jackal3166@gmail.com>
2024-03-17 02:26:37 +0200

    Create Location

--- components/Location/index.js
+++ components/Location/index.js
@@ -1,7 +1,12 @@
 export const Location = () => {
-    return (
-        <>
-            <h3>Location</h3>
-        </>
-    )
-}
+	return (
+		<section className='p-16 mb-32 mx-[210px]'>
+			<h3 className='mb-16 text-4xl font-bold text-center'>
+				Mentor&apos;s <span className='text-red-900'>Map</span>
+			</h3>
+			<div>
+				
+			</div>
+		</section>
+	);
+};
