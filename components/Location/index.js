2be52ce8891b61fa5214ab5c6b176f537bd2c569
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2024-03-18 22:14:28 +0100

    Update index.js

--- components/Location/index.js
+++ components/Location/index.js
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic';
 
 export const Location = () => {
     return (
@@ -6,8 +7,6 @@ export const Location = () => {
         </>
     )
 }
-
-import dynamic from 'next/dynamic';
 
 const DynamicMap = dynamic(() => import('./Map'), {
 	loading: () => <p>Loading...</p>,
