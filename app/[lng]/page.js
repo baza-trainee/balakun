75e72fcad07ad92ce969f66a1f3f4bf88b64ffad
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2024-03-19 19:47:11 +0100

    Create page.js

---
+++ app/[lng]/page.js
@@ -0,0 +1,32 @@
+import { About } from '@/components/About';
+import { Achivements } from '@/components/Achivements';
+import { Community } from '@/components/Community';
+import { Experience } from '@/components/Experience';
+import { Hero } from '@/components/Hero';
+import { Location } from '@/components/Location';
+import { Questions } from '@/components/Questions';
+import { Team } from '@/components/Team';
+import { YouTube } from '@/components/YouTube';
+
+/*This element is example, It can switch language*/
+//import LanguageChanger from "@/components/Internationalization/LanguageChanger";
+//import ExampleClient from "@/components/Internationalization/ExampleClient";
+
+//file's name, you can add other files in other components
+const i18nNamespaces = ['translation'];
+
+export default async function Home({ params: { lng } }) {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Achivements />
+      <Team />
+      <Location />
+      <YouTube />
+      <Experience />
+      <Questions />
+      <Community />
+    </>
+  );
+}
