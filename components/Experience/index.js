a21d66f3e3597bff5840cab0ae2e7112c1c821bb
Valera001 <jokerr192@gmail.com>
2024-03-20 18:31:44 +0200

    refactor:component "SwiperBtn"; refactor: swiper and swirep btn

--- components/Experience/index.js
+++ components/Experience/index.js
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import { ptsans } from "@/fonts";
-import { Navigation } from "swiper/modules";
-import { SwiperBtn } from "./SwiperBtn";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import ExperienceCard from "./ExperienceCard";
-import cardData from "./data.js";
+import { ptsans } from '@/fonts';
+import { Navigation } from 'swiper/modules';
+import { SwiperBtn } from '../Buttons/SwiperBtn';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import ExperienceCard from './ExperienceCard';
+import cardData from './data.js';
 
 export const Experience = () => {
   return (
-    <section className="">
+    <section className="mb-[66px]">
       <h1 className={`${ptsans.className} text-4xl font-bold text-center`}>
         Voice of<span className="text-red-900"> Experience</span>
       </h1>
@@ -23,8 +23,8 @@ export const Experience = () => {
           spaceBetween={16}
           slidesPerView={3}
           navigation={{
-            nextEl: ".s-button-next",
-            prevEl: ".s-button-prev",
+            nextEl: '.s-button-next',
+            prevEl: '.s-button-prev',
           }}
 
           // pagination={{ clickable: true }}
@@ -41,10 +41,10 @@ export const Experience = () => {
             </SwiperSlide>
           ))}
         </Swiper>
-        <div className="absolute rotate-180 -translate-y-1/2 s-button-next -left-16 top-2/4">
+        <div className="absolute duration-200 rotate-180 -translate-y-1/2 s-button-next -left-16 top-2/4 active:-translate-y-5">
           <SwiperBtn />
         </div>
-        <div className="absolute -translate-y-1/2 s-button-prev -right-14 top-2/4">
+        <div className="absolute duration-200 -translate-y-1/2 s-button-prev -right-14 top-2/4 active:-translate-y-5">
           <SwiperBtn />
         </div>
       </div>
